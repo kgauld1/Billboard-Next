38136a68e676e23bde895936d32e5365e5c0b239
Ethaniel <ethanhorowitz@ymail.com>
2020-08-17 20:20:36 +0000

    seeing if torch works on heroku

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import network
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
--- network.py
+++ network.py
@@ -13,3 +13,9 @@ charts.append(billboard.ChartData('hot-100', charts[0].previousDate))
 
 previous = charts[0][:10]
 current = charts[1][:10]
+
+model = torch.load('trainedNet.pth')
+model.eval()
+
+def get_next():
+	pass
