2bf63088836d61780e3f8283347c20d6738c2274
Jendy <jendyren@gmail.com>
2020-08-17 22:15:11 +0000

    LinkedIn Link

--- network.py
+++ network.py
@@ -18,4 +18,35 @@ model = torch.load('trainedNet.pth')
 model.eval()
 
 def get_next():
+	x = []
+	y_train = []
+
+	artists = {}
+
+	for i in range(len(charts) - 1):
+		x_train.append([])
+		y_train.append([])
+		for song in charts[i]:
+			if song.artist not in artists:
+				artists[song.artist] = random.random() * 10
+			x_train[-1].extend([artists[song.artist], song.peakPos, song.lastPos, song.weeks, song.rank, song.isNew])
+			found = False
+			for c in charts[i+1]:
+				if c.title == song.title:
+					y_train[-1].append(c.rank)
+					found = True
+					break
+			if not found:
+				y_train[-1].append(101)
+
+
+  #   if song in charts[i+1]:
+  #     print('sc')
+  # for song in charts[i+1]:
+  #   if song.artist not in artists:
+  #     artists[song.artist] = random.random() * 10
+  #   y_train[-1].append(song.rank)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 	pass
