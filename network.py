7e7e682cd75f827b08e2c4e37fe91ab93ff658d5
Ethaniel <ethanhorowitz@ymail.com>
2020-08-18 05:43:45 +0000

    added predictions

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import billboard
+import pickle
 import numpy as np
 import torch
 import torch.nn as nn
@@ -14,39 +15,29 @@ charts.append(billboard.ChartData('hot-100', charts[0].previousDate))
 previous = charts[0][:10]
 current = charts[1][:10]
 
-model = torch.load('trainedNet.pth')
+model = torch.load('bestNet.pth')
 model.eval()
+
+artists = pickle.load('artists.pickle')
 
 def get_next():
 	x = []
-	y_train = []
 
-	artists = {}
+	for song in charts[1]:
+		x.extend([artists[song.artist], song.peakPos, song.lastPos, song.weeks, song.rank, song.isNew])
 
-	for i in range(len(charts) - 1):
-		x_train.append([])
-		y_train.append([])
-		for song in charts[i]:
-			if song.artist not in artists:
-				artists[song.artist] = random.random() * 10
-			x_train[-1].extend([artists[song.artist], song.peakPos, song.lastPos, song.weeks, song.rank, song.isNew])
-			found = False
-			for c in charts[i+1]:
-				if c.title == song.title:
-					y_train[-1].append(c.rank)
-					found = True
-					break
-			if not found:
-				y_train[-1].append(101)
+	return torch.FloatTensor(np.array([x, x]))
+
+def predict():
+	outputs = model(get_next())[0]
+	newPos = np.argsort(list(outputs))
+	ranking = []
+	for p in newPos:
+		ranking.append(charts[1][p])
+	
+	return ranking
+		
+
+	
 
 
-  #   if song in charts[i+1]:
-  #     print('sc')
-  # for song in charts[i+1]:
-  #   if song.artist not in artists:
-  #     artists[song.artist] = random.random() * 10
-  #   y_train[-1].append(song.rank)
-
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-	pass
