ec238c197573f2b8a6ba9c5a68105f3a547df8b0
Ethaniel <ethanhorowitz@ymail.com>
2020-08-23 00:06:22 +0000

    tried jit load

--- network.py
+++ network.py
@@ -15,7 +15,7 @@ charts.append(billboard.ChartData('hot-100', charts[0].previousDate))
 previous = charts[0][:10]
 current = charts[1][:10]
 
-model = torch.load('bestNet.pth')
+model = torch.jit.load('bestNet.pth')
 model.eval()
 
 artists = pickle.load('artists.pickle')
