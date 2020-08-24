f4b26e48394672120c9182930f22a4732d17155f
Kevin Gauld <kgauld1@gmail.com>
2020-08-24 11:18:26 +0000

    idek (revert to last commit if i did something very wrong)

--- static/js/loadSongs.js
+++ static/js/loadSongs.js
@@ -8,49 +8,46 @@ fetch('/songs', {
 }).then(resp => resp.json()).then(json => {
 	let ranks = json.ranks;
 	for (let i=0; i<10; i++){
-		var song = document.createElement("div");
-		song.setAttribute("class", "song");
-
-		var songTitle = "Song title";
-		var artistName = "Artist name";
-
-		//OPTIMIZE THIS LATER
-		var rank = document.createElement("div");
-		rank.setAttribute("class", "rank");
-		rank.innerHTML = i + 1;
-
-		var songInfo = document.createElement("div");
-		songInfo.setAttribute("class", "song-info");
-		songInfo.innerHTML = `<b>${ranks[i].name}</b>${ranks[i].artist}`
-
-    var f = document.createElement("form");
-    f.setAttribute("method", "get");
-    f.setAttribute("target", "_blank");
-
-
-    var btn = document.createElement("BUTTON");
-    btn.setAttribute("class", "spotify-button");
-    btn.setAttribute("type", "submit");
-    f.setAttribute("action", "https://google.com");
-    f.appendChild(btn);
-
-
     let key = json.token
     let url = "https://api.spotify.com/v1/search?q=" + ranks[i].name.replace(/\s+/g, '%20').toLowerCase() + "&type=track&market=US&limit=1"
 
     
     getLink(url,key).then(function(data){
-      f.setAttribute("action", data);
+      var song = document.createElement("div");
+      song.setAttribute("class", "song");
+
+      var songTitle = "Song title";
+      var artistName = "Artist name";
+
+      //OPTIMIZE THIS LATER
+      var rank = document.createElement("div");
+      rank.setAttribute("class", "rank");
+      rank.innerHTML = i + 1;
+
+      var songInfo = document.createElement("div");
+      songInfo.setAttribute("class", "song-info");
+      songInfo.innerHTML = `<b>${ranks[i].name}</b>${ranks[i].artist}`
+
+      var f = document.createElement("form");
+      f.setAttribute("method", "get");
+      f.setAttribute("target", "_blank");
+
+      var btn = document.createElement("BUTTON");
+      btn.setAttribute("class", "spotify-button");
+      btn.setAttribute("type", "submit");
+      f.setAttribute("action", "data");
+      f.appendChild(btn);
+
+      song.appendChild(rank)
+      song.appendChild(songInfo)
+      song.appendChild(f)
+
+      ranking.appendChild(song);
+
     }).catch(function(error){
       console.log(error);
     })
-    
-
-		song.appendChild(rank)
-		song.appendChild(songInfo)
-    song.appendChild(f)
-
-		ranking.appendChild(song);
+		
 	}
 });
 
